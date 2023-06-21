39c449401befd2710772f6b4c01f6cd7158ce8ce
Robert Maier-Silldorff <Robert.Maier-Silldorff@iteratec.com>
2023-06-21 21:09:53 +0200

    add signal-example

--- src/app/pages/suche-page/suche-page.component.ts
+++ src/app/pages/suche-page/suche-page.component.ts
@@ -1,5 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, effect, inject, signal} from '@angular/core';
 import {Reise} from '../../models/common';
+import {ReiseService} from '../../services/reise.service';
+import {combineLatest, interval, lastValueFrom} from 'rxjs';
+import {toObservable, toSignal} from '@angular/core/rxjs-interop';
+import {initialState} from '../../+state/reducers';
+import {Store} from '@ngrx/store';
+import {selectAngebote} from '../../+state/selectors';
 
 @Component({
     selector: 'app-suche-page',
@@ -9,15 +15,51 @@ import {Reise} from '../../models/common';
 export class SuchePageComponent {
 
     reisen: Reise[];
-    filteredReisen: Reise[] = [];
-    destination = '';
-    nights = 1;
+    filteredReisen = signal([]);
+    destination = signal('');
+    nights = signal(1);
+
+    store = inject(Store);
+
+    reiseService = inject(ReiseService);
+
+    destination$ = toObservable(this.destination);
+    nights$ = toObservable(this.nights);
+
+    criteria$ = combineLatest([this.destination$, this.nights$, interval(1000)]);
+
+    // use rxjsinterop-api; create signal with initial value
+    criteria = toSignal(this.criteria$, {initialValue: ['test', 1, -1]});
+
+    // use rxjsinterop-api
+    critera2$ = toObservable(this.criteria);
+
+    // select Signal from store
+    angebote = this.store.selectSignal(selectAngebote);
+
+
+    constructor() {
+        effect(() => {
+            // trigger search
+            void this.search();
+        });
+
+        // automatic unsubscribe for signals
+        /*effect(() => {
+            console.log('Count ', this.criteria()[2]);
+        });*/
+
+        // automatic unsubscribe for observable
+        // this.critera2$.subscribe((value) => console.log('Count', value[2]));
+    }
+
 
     addToCart(item: Reise) {
         throw new Error('not implemented yet');
     }
 
-    search(): void {
-        throw new Error('not implemented yet');
+    async search(): Promise<void> {
+        const value = await lastValueFrom(this.reiseService.search(this.destination(), this.nights()));
+        this.filteredReisen.set(value);
     }
 }
